a70b1d63404c5652f86449b976249bf55eb410fd
amaldas <amaldasxda@gmail.com>
2019-09-10 13:37:46 -0400

    get rent

--- server.py
+++ server.py
@@ -13,20 +13,33 @@ model = pickle.load(open('model.pkl','rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/api',methods=['POST'])
-def predict():
-    # Get the data from the POST request.
-    data = request.get_json(force=True)
+@app.route('/getrent',methods=['POST'])
+def getrent():
 
-    # Make prediction using model loaded from disk as per the data.
+    # Get the data from the POST request.
+    unit = int(request.form['unitSelected'])
+    bed = int(request.form['bedSelected'])
+    bath = int(request.form['bathSelected'])
+    pet = int(request.form['petSelected'])
+    fur = int(request.form['furSelected'])
+    park = int(request.form['parSelected'])
+    air = int(request.form['airSelected'])
+    smo = int(request.form['smoSelected'])
+    region = int(request.form['regSelected'])
+
+    #data = request.get_json(force=True)
+    data = [unit,bed,bath,pet,fur,park,air,smo,region]
     print(data)
+    # Make prediction using model loaded from disk as per the data.
+    #print(data)
     #prediction = model.predict([[np.array(data['exp'])]])
-    prediction = model.predict([data['exp']])
+    prediction = model.predict([data])
 
     # Take the first value of prediction
     output = prediction[0]
-
-    return jsonify(output)
+    output = round(np.expm1(output),2)
+    rent = '$ '+str(output)
+    return jsonify(rent)
 
 if __name__ == '__main__':
     app.run(port=5003, debug=True)
